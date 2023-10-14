0a013cd46d426a3651cf067e7a8bd105a663d99d
architkumar314 <architkumar314@gmail.com>
2023-10-14 23:24:53 +0530

    Fixes for migration

--- Adonis work/user_registration/database/migrations/1690820168702_api_tokens.ts
+++ Adonis work/user_registration/database/migrations/1690820168702_api_tokens.ts
@@ -6,11 +6,15 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
 
-      table.increments('id').primary()
-      table.uuid('user_id').references('id').inTable('users').onDelete('CASCADE').notNullable()
+      table.uuid('id').primary()
+
+      table.uuid('user_id').references('id').inTable('users').notNullable()
       table.string('name').notNullable()
-      table.string('type').notNullable()
+      table.string('user_name').notNullable()
+      table.string('email').notNullable()
       table.string('token', 64).notNullable().unique()
+      table.string('password', 180).notNullable()
+      table.boolean('status').defaultTo(true).notNullable()
 
       table.timestamp('expires_at', { useTz: true }).nullable()
       table.timestamp('created_at', { useTz: true }).notNullable()
